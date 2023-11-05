46276ea457d3f8ad5a928ba8ea3ddd4be5b34e7b
Shawn Nassabi <san7522@nyu.edu>
2023-11-04 22:05:49 -0400

    worked on todo

--- src/App.jsx
+++ src/App.jsx
@@ -1,35 +1,63 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
+import { useState } from "react"
+import "./styles.css"
 
-function App() {
-  const [count, setCount] = useState(0)
+export default function App() { // can only return one component at once, so use a div
+  // or fragment to return multuple at once
+  const [newItem, setNewItem] = useState("");
+  const [todos, setTodos] = useState([]);
+  // What is state?
+  // In react, state is immutable. setNewItem("adwadd") will set newItem to the input
+  // when you change a state variable, it reruns everything
+  function handleSubmit(e) {
+    e.preventDefault();
+    setTodos((currentTodos) => {
+      return [...currentTodos, {id : crypto.randomUUID(), title: newItem, completed: false}];
+    })
 
-  return (
-    <>
-      <div>
-        <a href="https://vitejs.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
+    setNewItem(""); // Empties the input box after every entry
+  }
+
+  function toggleTodo(id, completed){
+    setTodos(currentTodos => {
+      return currentTodos.map(todo => {
+        if (todo.id === id){
+          return {...todo, completed}
+        }
+        return todo
+      })
+    })
+  }
+ 
+  return ( // we used a fragment <> </>
+    <> 
+      <form onSubmit={handleSubmit} className="new-item-form">
+        <div className="form-row">
+          <label htmlFor="item">New Item</label>
+          <input 
+            value={newItem}
+            onChange={e => setNewItem(e.target.value)}
+            type="text" 
+            id="item" 
+          />
+        </div>
+        <button className="btn">Add</button>
+      </form>
+      <h1>Todo List:</h1>
+      <ul className="list">
+        {todos.map(todo => {
+          return (
+            <li key={todo.id}>
+              <label>
+                <input type="checkbox" checked={todo.completed} 
+                onChange={e => toggleTodo(todo.id, e.target.checked)} />
+                {todo.title}
+              </label>
+              <button className="btn btn-danger">Delete</button>
+            </li>
+          )
+        })}
+      </ul>
     </>
   )
-}
 
-export default App
+}
